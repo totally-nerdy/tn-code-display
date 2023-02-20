e02b56b05dd2de7c95982d92a3376cb1b52429c1
Patrick Pittich-Rinnerthaler <patrick@totally-nerdy.com>
2023-02-20 10:55:36 +0000

    Added inline svg loading image

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ class CodeDisplay extends HTMLElement {
   constructor () {
     super()
 
+    const LOADING_IMAGE = '<svg fill="%LOADING_COLOR%" height="20" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg"><circle cx="15" cy="15" r="15"><animate attributeName="r" begin="0s" calcMode="linear" dur="0.8s" from="15" repeatCount="indefinite" to="15" values="15;9;15"/><animate attributeName="fill-opacity" begin="0s" calcMode="linear" dur="0.8s" from="1" repeatCount="indefinite" to="1" values="1;.5;1"/></circle><circle cx="60" cy="15" r="9" fill-opacity=".3"><animate attributeName="r" begin="0s" calcMode="linear" dur="0.8s" from="9" repeatCount="indefinite" to="9" values="9;15;9"/><animate attributeName="fill-opacity" begin="0s" calcMode="linear" dur="0.8s" from="0.5" repeatCount="indefinite" to="0.5" values=".5;1;.5"/></circle><circle cx="105" cy="15" r="15"><animate attributeName="r" begin="0s" calcMode="linear" dur="0.8s" from="15" repeatCount="indefinite" to="15" values="15;9;15"/><animate attributeName="fill-opacity" begin="0s" calcMode="linear" dur="0.8s" from="1" repeatCount="indefinite" to="1" values="1;.5;1"/></circle></svg>'
+
     this._code = this.firstElementChild.value
     this._language = this.getAttribute('data-language')
     this._style = this.getAttribute('data-style') || 'monokai-sublime'
+    this._loadingColor = this.getAttribute('data-loading-color') || '#10c100'
     this._scripts = [
       'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js',
       'https://www.unpkg.com/prettier@2.8.3/standalone.js'
@@ -20,6 +23,8 @@ class CodeDisplay extends HTMLElement {
       _style.innerHTML = `@import '//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/${self._style}.min.css';`
       _template.innerHTML = `<pre><code class="language-${self._language}"></code></pre>`
 
+      // Remove loading image again before adding the code box
+      self.shadowRoot.querySelector('svg').remove()
       self.shadowRoot.appendChild(_template.content.cloneNode(true))
       self.shadowRoot.appendChild(_style)
       self.shadowRoot.querySelector('pre code').appendChild(document.createTextNode(_prettyCode))
@@ -67,6 +72,12 @@ class CodeDisplay extends HTMLElement {
     }
 
     this.attachShadow({ mode: 'open' })
+
+    // Display loading image
+    const _loaderTemplate = document.createElement('template')
+    _loaderTemplate.innerHTML = LOADING_IMAGE.replace('%LOADING_COLOR%', this._loadingColor)
+    this.shadowRoot.appendChild(_loaderTemplate.content.cloneNode(true))
+
     this._loadScriptsAndDisplay(this)
   }
 }
