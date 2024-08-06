31a779fbc956ca0090e15f1471526dc7ae88c7cb
Patrick Pittich-Rinnerthaler <patrick@totally-nerdy.com>
2024-08-06 10:31:00 +0100

    Release v1.4.0; see CHANGELOG.md for details

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 1.4.0 (Aug 6, 2024)
+* Started CHANGELOG.md
+* Bumped node and npm to latest LTS versions in package.json
+* Made prettier parsing optional (e.g. unknown language given)
+* Added event listener on the original textarea to handle input changes
--- index.js
+++ index.js
@@ -6,23 +6,22 @@ class CodeDisplay extends HTMLElement {
     const CLIPBOARD_IMAGE = '<svg fill="#000" width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z"/><path d="M3.5 1h.585A1.5 1.5 0 0 0 4 1.5V2a1.5 1.5 0 0 0 1.5 1.5h5A1.5 1.5 0 0 0 12 2v-.5q-.001-.264-.085-.5h.585A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1"/></svg>'
     const CLIPBOARD_CHECK_IMAGE = '<svg fill="#000" width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5"/><path d="M4.085 1H3.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1h-.585q.084.236.085.5V2a1.5 1.5 0 0 1-1.5 1.5h-5A1.5 1.5 0 0 1 4 2v-.5q.001-.264.085-.5m6.769 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708.708"/></svg>'
 
-    this._code = this.firstElementChild.value
+    this._textarea = this.firstElementChild
+    this._code = this._textarea.value
     this._prettyCode = ''
     this._language = this.getAttribute('data-language')
     this._style = this.getAttribute('data-style') || 'monokai-sublime'
     this._loadingColor = this.getAttribute('data-loading-color') || '#10c100'
     this._clipboard = Boolean(this.getAttribute('data-clipboard') || false)
     this._scripts = [
-      'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js',
-      'https://www.unpkg.com/prettier@2.8.3/standalone.js'
+      'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js'
     ]
-    this._display = function (self) {
-      self._prettyCode = prettier.format(self._code, {
-        parser: self._parser,
-        plugins: prettierPlugins
-      })
+    this._removeLoadingImage = function (self) {
+      // Remove loading image again before adding the code box
+      self.shadowRoot.querySelector('svg').remove()
+    }
+    this._appendStyles = function (self) {
       const _style = document.createElement('style')
-      const _template = document.createElement('template')
 
       _style.innerHTML =
         `
@@ -37,21 +36,34 @@ class CodeDisplay extends HTMLElement {
           right: 0;
           margin-top: 4px;
           margin-right: 4px;
-          padding: 10px;
+          padding: 4px;
           background-color: #fff;
           border-radius: 2px;
           cursor: pointer;
         }
         `
+
+      self.shadowRoot.appendChild(_style)
+    }
+    this._display = function (self) {
+      if (typeof prettier !== 'undefined') {
+        self._prettyCode = prettier.format(self._code, {
+          parser: self._parser,
+          plugins: prettierPlugins
+        })
+      } else {
+        self._prettyCode = self._code
+      }
+      const _template = document.createElement('template')
+
       _template.innerHTML = `<pre id="container"><code class="language-${self._language}"></code><div id="clipboard">${CLIPBOARD_IMAGE}</div></pre>`
 
-      // Remove loading image again before adding the code box
-      self.shadowRoot.querySelector('svg').remove()
       self.shadowRoot.appendChild(_template.content.cloneNode(true))
-      self.shadowRoot.appendChild(_style)
       self.shadowRoot.querySelector('pre code').appendChild(document.createTextNode(self._prettyCode))
 
-      // Adding event listeners
+      /**
+       * Adding event listeners for clipboard functionality
+       */
       if (this._clipboard) {
         self.shadowRoot.querySelector('pre').addEventListener('mouseenter', this._onMouseEnter)
         self.shadowRoot.querySelector('pre').addEventListener('mouseleave', this._onMouseLeave)
@@ -72,16 +84,21 @@ class CodeDisplay extends HTMLElement {
         if (self._scripts.length) {
           self._loadScriptsAndDisplay(self)
         } else {
+          self._appendStyles(self)
+          self._removeLoadingImage(self)
           self._display(self)
         }
       }
       el.src = script
     }
-    this._onMouseEnter = function (self) {
-      self.target.querySelector('div[id="clipboard"]').style.display = 'block'
+    /**
+     * Event listeners
+     */
+    this._onMouseEnter = function (e) {
+      e.target.querySelector('div[id="clipboard"]').style.display = 'block'
     }
-    this._onMouseLeave = function (self) {
-      const clipboard = self.target.querySelector('div[id="clipboard"]')
+    this._onMouseLeave = function (e) {
+      const clipboard = e.target.querySelector('div[id="clipboard"]')
       clipboard.style.display = 'none'
 
       const _template = document.createElement('template')
@@ -89,6 +106,7 @@ class CodeDisplay extends HTMLElement {
       clipboard.replaceChildren(_template.content.cloneNode(true))
     }
     this._onClickClipboard = function () {
+      // this = CodeDisplay object
       const clipboard = this.shadowRoot.querySelector('div[id="clipboard"]')
 
       // navigator.clipboard.writeText(this._code) // copy original code
@@ -98,31 +116,45 @@ class CodeDisplay extends HTMLElement {
       _template.innerHTML = CLIPBOARD_CHECK_IMAGE
       clipboard.replaceChildren(_template.content.cloneNode(true))
     }
+    this._onChange = function () {
+      // this = CodeDisplay object
+      this.shadowRoot.querySelector('pre').remove()
+      this._code = this._textarea.value
+      this._display(this)
+    }
 
     switch (this._language) {
       case 'css':
+        this._scripts.push('https://www.unpkg.com/prettier@2.8.3/standalone.js')
         this._scripts.push('https://www.unpkg.com/prettier@2.8.3/parser-postcss.js')
         this._parser = 'css'
         break
       case 'scss':
+        this._scripts.push('https://www.unpkg.com/prettier@2.8.3/standalone.js')
         this._scripts.push('https://www.unpkg.com/prettier@2.8.3/parser-postcss.js')
         this._parser = 'scss'
         break
       case 'less':
+        this._scripts.push('https://www.unpkg.com/prettier@2.8.3/standalone.js')
         this._scripts.push('https://www.unpkg.com/prettier@2.8.3/parser-postcss.js')
         this._parser = 'less'
         break
       case 'javascript':
+        this._scripts.push('https://www.unpkg.com/prettier@2.8.3/standalone.js')
         this._scripts.push('https://www.unpkg.com/prettier@2.8.3/parser-meriyah.js')
         this._parser = 'meriyah'
         break
       case 'html':
+        this._scripts.push('https://www.unpkg.com/prettier@2.8.3/standalone.js')
         this._scripts.push('https://www.unpkg.com/prettier@2.8.3/parser-html.js')
         this._parser = 'html'
         break
     }
 
     this.attachShadow({ mode: 'open' })
+
+    // Adding event listener for changes on input textarea
+    this._textarea.addEventListener('input', this._onChange.bind(this))
 
     // Display loading image
     const _loaderTemplate = document.createElement('template')
